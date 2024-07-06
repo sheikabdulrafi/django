6129a3834f909d32e2a6ab9b871649ff28c2d37c
umarmohammad96 <onesownumar@gmail.com>
2024-07-06 22:57:19 +0530

    verce file and bash file modified

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "started build"
+
+# Ensure Python and pip are available
+export PATH="/vercel/path0/.python/bin:$PATH"
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic
-echo "done collecting static files"
-echo 'build complete'
+python manage.py collectstatic --noinput
