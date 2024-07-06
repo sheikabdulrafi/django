820cc3786ed032b34fd9eaa11459e93ae8c21cf7
umarmohammad96 <onesownumar@gmail.com>
2024-07-06 22:25:13 +0530

    modified build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Ensure Python and pip are available
+export PATH="/usr/local/bin:$PATH"
+
+# Install dependencies
 pip install -r requirements.txt
-python3.11 manage.py collectstatic
+
+# Collect static files
+python3.11 manage.py collectstatic --noinput
